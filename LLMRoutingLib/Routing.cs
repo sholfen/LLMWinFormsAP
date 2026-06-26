3e60b2085558dc1326005cb9c0c6ede6e11883f7
Peter Liang <sholfen@gmail.com>
2026-06-26 13:32:04 +0800

    add LLM routing project

---
+++ LLMRoutingLib/Routing.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.AI;
+using OllamaSharp.Models;
+
+namespace LLMRoutingLib
+{
+    public enum ModelType
+    {
+        Llama3,
+        Mistral,
+        OpenAI,
+        AzureOpenAI,
+        Claude,
+        Other
+    }
+
+    public class Routing
+    {
+        private string _modelName = string.Empty;
+        private IChatClient _chatClient;
+
+        public Routing() : this(@"llama3.1:latest")
+        {
+
+        }
+
+        public Routing(string modelName)
+        {
+            _modelName = modelName;
+            _chatClient = new OllamaSharp.OllamaApiClient(new Uri("http://localhost:11434"), _modelName);
+        }
+
+        public string GetModelName(string prompt)
+        {
+            string systemPrompt = "You are a helpful assistant.";
+
+            return _modelName;
+        }
+    }
+}
